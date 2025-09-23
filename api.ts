9c77e226637756b0b38d45ec14568d28639b672b
makdiel <makdiel.tabora@hotmail.com>
2025-09-22 18:05:24 -0600

    update
    usando contenedores

--- api.ts
+++ api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 const api = axios.create({ 
-  baseURL: "http://192.168.1.34:5000/api", 
+  baseURL: "http://192.168.10.105:25060/api", 
   // baseURL : 'https://abcd1234.ngrok.io/api',
 
-  timeout: 5000 
+  timeout: 25060 
 });
 export default api;
